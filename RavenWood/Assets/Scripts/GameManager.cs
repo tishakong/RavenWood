b198b5a455a0491ccb80bd09a07e6f5c6fbe640d
tishakong <leehn45@naver.com>
2024-02-24 16:49:53 +0900

    feat/제한시간 타이머 UI 생성
    UI 크기 협의 필요

--- RavenWood/Assets/Scripts/GameManager.cs
+++ RavenWood/Assets/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour
     public ClickManager clickManager;
     public GameObject talkPanel;        // 게임 인포창
     public GameObject Inventory;        // 인벤토리 UI
+    public Text timerText;              // 제한 시간 확인 텍스트
+    private float timeRemaining;        // 잔여 시간 관리 변수
     public TextMeshProUGUI talkText;    // 게임창에 뜨는 텍스트
     public GameObject scanObject;       // 플레이어가 조사한 대상
     public bool isAction;               // 상태 저장용 변수
@@ -20,6 +22,23 @@ public class GameManager : MonoBehaviour
     {
         Inventory.SetActive(false);
         isInventoryActivate = false;
+        timeRemaining = 600.0f;
+    }
+
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            int minutes = Mathf.FloorToInt(timeRemaining / 60);
+            int seconds = Mathf.FloorToInt(timeRemaining % 60);
+            string timeString = string.Format("{0:00}:{1:00}", minutes, seconds);
+            timerText.text = timeString;
+        }
+        else
+        {
+            timerText.text = "제한 시간 초과(게임오버)";
+        }
     }
 
     public void Action(GameObject scanObj)
