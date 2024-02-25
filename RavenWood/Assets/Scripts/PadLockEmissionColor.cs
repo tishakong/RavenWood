0aa87177c5d626087995924bd526488a43727cbe
chaekang <herry612@naver.com>
2024-02-25 17:48:08 +0900

    feat/자물쇠 따기
    feat/자물쇠 번호 맞추기 및 열리는 애니메이션 추가

--- RavenWood/Assets/Scripts/PadLockEmissionColor.cs
+++ RavenWood/Assets/Scripts/PadLockEmissionColor.cs
@@ -11,30 +11,33 @@ public class PadLockEmissionColor : MonoBehaviour
     [HideInInspector]
     public bool _isSelect;
 
-    //[SerializeField] private float _timeBlinking = 0.5f;
-
     private void Awake()
     {
         tb = FindObjectOfType<TimeBlinking>();
     }
+
     void Start()
     {
         _myRuller = gameObject;
     }
 
-
     public void BlinkingMaterial()
     {
         _myRuller.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
-        
+
         if (_isSelect)
         {
             _myRuller.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.Lerp(Color.clear, Color.yellow, Mathf.PingPong(Time.time, tb.blinkingTime)));
         }
-        if (_isSelect == false)
+        if (!_isSelect)
         {
             _myRuller.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.clear);
         }
+    }
 
+    public void DisableBlinkingMaterial()
+    {
+        // Disable blinking material
+        _myRuller.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
     }
 }
